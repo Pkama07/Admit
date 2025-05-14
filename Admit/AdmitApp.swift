cdb026ba8be754345973f6381f37a7fbff47f11e
Pkama07 <pradyun774@gmail.com>
2025-05-14 13:27:48 -0400

    Initial Commit

---
+++ Admit/AdmitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmitApp.swift
+//  Admit
+//
+//  Created by Pradyun Kamaraju on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct AdmitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdmitTests/AdmitTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdmitTests.swift
+//  AdmitTests
+//
+//  Created by Pradyun Kamaraju on 5/14/25.
+//
+
+import Testing
+@testable import Admit
+
+struct AdmitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
